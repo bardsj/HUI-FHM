fabdf9a5859de5d68025249e38838677a9f1bcce
JackBardsley <jack.bardsley95@gmail.com>
2021-02-25 21:07:26 +0000

    package changes

--- setup.py
+++ setup.py
@@ -1,6 +1,7 @@
-from setuptools import setup
+from setuptools import setup,find_packages
 
-setup(name='HighUtilityItemsetMining',
+setup(name='huimining',
       version=0.1,
       url='https://github.com/JackBardsley/HUI-FHM',
+      packages = find_packages(),
       zip_safe=False)
